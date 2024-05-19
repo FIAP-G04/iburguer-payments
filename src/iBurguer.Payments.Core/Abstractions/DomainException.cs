a482ca07232f9e24adc53644164ad67eb3f3cd4b
Vinicius Saeta <vinisaeta@gmail.com>
2024-05-18 23:36:06 -0300

    :white_check_mark: mais testes

--- src/iBurguer.Payments.Core/Abstractions/DomainException.cs
+++ src/iBurguer.Payments.Core/Abstractions/DomainException.cs
@@ -3,8 +3,6 @@ namespace iBurguer.Payments.Core.Abstractions;
 public abstract class DomainException<TException>(string message) : 
     ApplicationException(message) where TException : DomainException<TException>, new()
 {
-    public static TException New => new();
-
     public static void ThrowIf(bool condition)
     {
         if (condition) throw new TException();
@@ -14,11 +12,4 @@ public abstract class DomainException<TException>(string message) :
     {
         if (t is null) throw new TException();
     }
-    
-    public static void ThrowIfEmpty(string t)
-    {
-        if (string.IsNullOrEmpty(t)) throw new TException();
-    }
-
-    public static IDomainEvent Throw() => throw new TException();
 }
